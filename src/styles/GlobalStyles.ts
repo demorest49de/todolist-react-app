75707901812227d381865fdb3022774bc5b1d508
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-05 10:40:27 +0300

    Changes .gitignore App.css App.test.tsx App.tsx favicon.ico GlobalStyles.ts index.css index.html index.tsx logo.svg logo192.png logo512.png manifest.json package.json react-app-env.d.ts README.md robots.txt setupTests.ts Theme.Styled.ts Todolist.tsx tsconfig.json yarn.lock

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,95 @@
+import {createGlobalStyle} from "styled-components";
+import theme from "./Theme.Styled";
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+
+    //  esli nuzhno posmotret kakoy block vilazit na adaptive
+
+    outline: 1px solid indianred;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-family: 'Ubuntu Condensed', 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    min-width: 320px;
+    min-height: 100vh;
+    background-color: ${theme.backgroundColor.primary};
+    color: ${theme.colors.white};
+  }
+
+  input, textarea {
+    color: ${theme.colors.white};
+  }
+
+  picture {
+    width: 100%;
+    height: auto;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  img, label {
+    user-select: none;
+  }
+
+  button {
+    cursor: pointer;
+    padding: 0;
+    background-color: unset;
+    border: none;
+    color: ${theme.colors.white}
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  ul, ol, h1, h2, h3, h4, p, figure, fieldset, blockquote {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul, ol {
+    padding: 0;
+    list-style: none;
+  }
+
+  li a {
+    display: block;
+  }
+
+  .visually-hidden {
+    position: absolute !important;
+    clip: rect(1px 1px 1px 1px);
+    clip: rect(1px, 1px, 1px, 1px);
+    padding: 0 !important;
+    border: 0 !important;
+    height: 1px !important;
+    width: 1px !important;
+    overflow: hidden;
+  }
+
+  address {
+    font-style: unset;
+  }
+`
---
+++ src/styles/Theme.Styled.ts
@@ -0,0 +1,34 @@
+const theme = {
+    colors: {
+        white: "#FFFFFF",
+        accent: "#7572D5",
+        greyOne: "#39393C",
+        greyTwo: "#4A4A4A",
+        greyThree: "#495057",
+    },
+    fonts: {
+        JosefinSans: "Josefin Sans",
+    },
+    fontWeight: {
+        regular: "400",
+        bold: "700",
+    },
+    backgroundColor: {
+        primary: "#1F1F20",
+        secondary: "#2d2d2f",
+        third: "#252527",
+    },
+
+    media: {
+        desktop1169px: "screen and (max-width: 1169px)",
+        desktop971px: "screen and (max-width: 971px)",
+        tablet: "screen and (max-width: 768px)",
+        mobile689px: "screen and (max-width: 689px)",
+        mobile597px: "screen and (max-width: 597px)",
+        mobile: "screen and (max-width: 576px)",
+        mobile459px: "screen and (max-width: 459px)",
+        mobile349px: "screen and (max-width: 349px)",
+    }
+}
+
+export default theme;
